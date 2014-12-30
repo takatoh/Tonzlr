16ca8b9358631a7cf455bf94845334335b2236e8
takatoh <takatoh.m@gmail.com>
2014-12-30 20:33:41 +0900

    Load config/site_config.yaml.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  SITE_CONFIG = YAML.load_file("config/site_config.yml")[Rails.env]
+
 end
