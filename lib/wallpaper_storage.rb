8dbfaa09ae71db12ad11e30a53a1d9711952d229
takatoh <takatoh.m@gmail.com>
2014-12-30 15:36:40 +0900

    lib/wallpaper_storage.rb: New.

---
+++ lib/wallpaper_storage.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+require 'pathname'
+
+
+class WallpaperStorage
+
+  @@file_dir = "files"
+  @@sample_dir = "samples"
+  @@thumbnail_dir = "thumbs"
+
+
+  def initialize(storage_dir)
+    @storage_dir = Pathname.new(storage_dir)
+  end
+
+  def store_wallpaper(wp_id, file, file_name)
+    wp_path = @storage_dir + @@file_dir + wp_id.to_s + file_name
+    FileUtils.mkdir_p(wp_path.parent)
+    FileUtils.cp(file, wp_path)
+    wp_path.to_s
+  end
+
+end   # of WallpaperStorage
