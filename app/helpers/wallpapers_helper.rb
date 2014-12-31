31f6d20efa7445a048d30db816cc2a349629f2e8
takatoh <takatoh.m@gmail.com>
2014-12-31 16:44:40 +0900

    Fix bug in wallpaper page.

--- app/helpers/wallpapers_helper.rb
+++ app/helpers/wallpapers_helper.rb
@@ -1,7 +1,19 @@
 module WallpapersHelper
 
+  def sample_path(wp)
+    "/wallpapers/sample/#{wp.id.to_s}"
+  end
+
   def thumbnail_path(wp)
     "/wallpapers/thumbnail/#{wp.id.to_s}"
+  end
+
+  def url_to_image(img)
+    "/images/#{img.wallpaper_id.to_s}/#{File.basename(img.path)}"
+  end
+
+  def time_to_date(time)
+    time.strftime("%Y-%m-%d")
   end
 
 end
