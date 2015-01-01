15b4c74f378ce179c5bf273f15995df3f0a16659
takatoh <takatoh.m@gmail.com>
2015-01-01 13:51:58 +0900

    config/routes.rb: Root to wallpapers#index.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'wallpapers/sample/:id' => "wallpapers#sample"
   get 'wallpapers/thumbnail/:id' => "wallpapers#thumbnail"
 
+  root :to => "wallpapers#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
